03604d8dfaa0d56c088492d6308cd6dbd29700d3
Ziyu Wang <zwang@lens10.com.au>
2015-03-18 13:53:01 +1100

    add wechat share

---
+++ assets/js/littlehorse.js
@@ -0,0 +1,58 @@
+/* wechat share */
+var wechatShare = function (layer) {
+  'user strict';
+
+  if (!layer) return;
+
+  var isWechatBrowser = /micromessenger/i.test(navigator.userAgent);
+
+  var show = function () {
+    if (isWechatBrowser) {
+      layer.style.display = 'block';
+    }
+  };
+
+  var hide = function () {
+    if (isWechatBrowser) {
+      layer.style.display = 'none';
+    }
+  };
+
+  var mute = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  layer.addEventListener('touchstart', mute);
+  layer.addEventListener('touchmove', mute);
+  layer.addEventListener('touchend', hide);
+  layer.addEventListener('click', hide);
+
+  return {
+    isWechatBrowser: isWechatBrowser,
+    show: show,
+    hide: hide,
+  };
+};
+
+var fn = function () {
+  var $buttons = $('section.share > a'),
+  $button = $('section.share .fa-wechat').parent().hide(),
+  $layer = $('section.share div#wechat-share-layer'),
+  wshare = wechatShare($layer[0]);
+
+  if (wshare.isWechatBrowser) {
+    $buttons.toggle();
+  }
+
+  if (wshare && $button[0]) {
+    $button.on('click', function (event) {
+      wshare.show();
+      event.preventDefault();
+    });
+  }
+}
+
+$(function () {
+  fn();
+});
